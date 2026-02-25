c0b37fddb81dcec5ae7189eea9848623d9caf3c6
loicmaes <production@maesloic.fr>
2026-02-25 13:02:36 +0100

    :rocket: setup default navigation sidebar

---
+++ app/components/navigation/default/index.ts
@@ -0,0 +1,96 @@
+import type { NavigationGroup, NavigationItem } from "~/types/primitives/sidebar";
+import { Home, Folder, Flag, Zap, Gauge, Brain, Ticket, MessageCircleWarning, LibraryBig, Settings } from "lucide-vue-next";
+
+export interface DefaultSidebarItemProps {
+  index: number;
+  item: NavigationItem;
+}
+
+export interface DefaultSidebarProps {
+  showSearch?: boolean;
+}
+
+export const sidebarContent = computed<NavigationGroup[]>(() => {
+  const { t } = useNuxtApp().$i18n;
+
+  return [
+    {
+      type: "group",
+      label: t("navigation.deployment.label"),
+      children: [
+        {
+          type: "item",
+          label: t("navigation.deployment.programs"),
+          path: "/deployment/programs",
+          icon: Folder,
+        },
+        {
+          type: "item",
+          label: t("navigation.deployment.courses"),
+          path: "/deployment/journeys",
+          icon: Flag,
+        },
+        {
+          type: "item",
+          label: t("navigation.deployment.actions"),
+          path: "/deployment/actions",
+          icon: Zap,
+        },
+      ],
+    },
+    {
+      type: "group",
+      label: t("navigation.analytics.label"),
+      children: [
+        {
+          type: "item",
+          label: t("navigation.analytics.usage"),
+          path: "/analytics/usage",
+          icon: Gauge,
+        },
+        {
+          type: "item",
+          label: t("navigation.analytics.ai"),
+          path: "/analytics/ai",
+          icon: Brain,
+        },
+      ],
+    },
+    {
+      type: "group",
+      label: t("navigation.support.label"),
+      children: [
+        {
+          type: "item",
+          label: t("navigation.support.tickets"),
+          path: "/support/tickets",
+          icon: Ticket,
+        },
+        {
+          type: "item",
+          label: t("navigation.support.abuses"),
+          path: "/support/abuses",
+          icon: MessageCircleWarning,
+        },
+      ],
+    },
+    {
+      type: "group",
+      label: t("navigation.administration.label"),
+      children: [
+        {
+          type: "item",
+          label: t("navigation.administration.content-library"),
+          path: "/administration/content-library",
+          icon: LibraryBig,
+        },
+        {
+          type: "item",
+          label: t("navigation.administration.settings"),
+          path: "/administration/settings",
+          icon: Settings,
+        },
+      ],
+    },
+  ];
+});
--- app/layouts/default.vue
+++ app/layouts/default.vue
@@ -1,9 +1,29 @@
 <script setup lang="ts">
 import LayoutRoot from "~/components/composing/LayoutRoot.vue";
+import GlobalSidebar from "~/components/navigation/default/GlobalSidebar.vue";
+import GettingHelp from "~/components/support/GettingHelp.vue";
 </script>
 
 <template>
   <LayoutRoot>
-    <NuxtPage />
+    <UiSidebarProvider>
+      <GlobalSidebar show-search />
+
+      <UiSidebarInset class="px-4 md:pl-2 flex flex-col max-h-dvh! overflow-y-auto">
+        <header class="shrink-0 sticky top-0 py-4 flex items-center gap-2 bg-background z-50">
+          <UiSidebarTrigger />
+
+          <!-- todo: breadcrumbs - loic -->
+
+          <div class="ml-auto flex items-center">
+            <GettingHelp />
+          </div>
+        </header>
+
+        <main class="p-2 pt-0 flex-1 flex flex-col min-h-0">
+          <NuxtPage />
+        </main>
+      </UiSidebarInset>
+    </UiSidebarProvider>
   </LayoutRoot>
 </template>
