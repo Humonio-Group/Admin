b19bc3ca8593c16e1e8d6a133f4f2a6d8907a275
loicmaes <97891718+loicmaes@users.noreply.github.com>
2026-06-04 11:34:15 +0200

    :rocket: add pagination utils

---
+++ app/composables/usePagination.ts
@@ -0,0 +1,23 @@
+export function usePagination(cb: (val: number) => Promise<unknown> | unknown) {
+  const { page: pageParam } = useRoute().query;
+  const page = pageParam ? Number(pageParam) : 1;
+  const defaultPage = page >= 1 ? page : 1;
+
+  const activePage = ref<number>(defaultPage);
+  watch(activePage, async (val) => {
+    const p = val ? (val >= 1 ? val : 1) : 1;
+    navigateTo({
+      query: {
+        page: p,
+      },
+      replace: true,
+    });
+    await cb(p);
+  }, { immediate: true });
+
+  async function reload() {
+    cb(activePage.value);
+  }
+
+  return { activePage, reload };
+}
