31d413d45ca13008d0c74a9414d31e6177f0bfc8
loicmaes <97891718+loicmaes@users.noreply.github.com>
2026-06-04 10:59:31 +0200

    :rocket: add search param in url

--- app/composables/useDebounceSearch.ts
+++ app/composables/useDebounceSearch.ts
@@ -1,7 +1,30 @@
 import { useDebounceFn } from "@vueuse/core";
 
 export function useDebounceSearch(cb: (search: string) => Promise<unknown> | unknown) {
-  const search = ref<string>("");
+  const { query } = useRoute();
+  const searchParam = query.q as string || query.search as string || "";
+
+  const search = ref<string>(searchParam);
+  watch(search, (val) => {
+    if (!val) {
+      navigateTo({
+        query: {
+          search: undefined,
+          q: undefined,
+        },
+        replace: true,
+      });
+      return;
+    }
+
+    const param = query.search ? "search" : "q";
+    navigateTo({
+      query: {
+        [param]: val,
+      },
+      replace: true,
+    });
+  });
   watch(search, useDebounceFn((val: string) => cb(val)));
 
   async function reload() {
